6c156388cc4c5ffa81b71204b4c9a92cf02928cb
Tony Skorik <just.skorik@gmail.com>
2025-11-08 01:12:30 +0000

    Fix resolving IOptionsSnapshot from root provider;

--- tests/Aer.QdrantClient.Tests/Base/QdrantTestsBase.cs
+++ tests/Aer.QdrantClient.Tests/Base/QdrantTestsBase.cs
@@ -108,7 +108,8 @@ public class QdrantTestsBase
                 registerAsInterface: false
             );
         }
-        ServiceProvider = services.BuildServiceProvider();
+        
+        ServiceProvider = services.BuildServiceProvider(validateScopes: true);
     }
 
     private string GetQdrantVersionFromEnvFile()
